2c59bc3752c216b075ddfc9406cb28a5c848d390
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-29 15:49:59 +0000

    Change Like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,10 @@ class LikesController < ApplicationController
   def new; end
 
   def create
-    @like = Like.create(author_id: params[:user_id], post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.new
+    @like.author_id = params[:user_id]
+    
     redirect_to user_post_path(params[:user_id], params[:post_id]) if @like.save
   end
 end
