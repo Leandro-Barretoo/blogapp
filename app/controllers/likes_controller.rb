1fce31563542de9fec9ce627b5e93462697c5c41
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-29 03:21:33 +0000

    Add like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,10 @@
 class LikesController < ApplicationController
+    def new; end
+
+    def create
+        @like = Like.create(author_id: params[:user_id], post_id: params[:post_id])
+        if @like.save
+            redirect_to user_post_path(params[:user_id], params[:post_id])
+        end
+    end
 end
