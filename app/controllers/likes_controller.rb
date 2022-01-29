1fbf1bb926ce615602cf21b25d0231a9c4f01533
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-29 15:50:38 +0000

    Fix rubocop offenses

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = @post.likes.new
     @like.author_id = params[:user_id]
-    
+
     redirect_to user_post_path(params[:user_id], params[:post_id]) if @like.save
   end
 end
